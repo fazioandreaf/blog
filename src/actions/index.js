4177b063cb890c3891ae3a5fb7b3a205102be7ce
Andrea Fazio <andrea.fazio@thelevelgroup.com>
2022-06-10 16:57:42 +0200

    Lodash memoize

--- src/actions/index.js
+++ src/actions/index.js
@@ -18,9 +18,11 @@ export const fetchPost = () => {
 }
 
 export const fetchUser = (id) => {
-    return async (dispatch) => {
-        const {data} = await jsonPlaceholder.get(`/users/${id}`)
-
-        dispatch({type: 'FETCH_USER', payload: data })
-    }
+    return (dispatch) => { _fetchUser(id, dispatch)}
 }
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
+    const {data} = await jsonPlaceholder.get(`/users/${id}`)
+
+    dispatch({type: 'FETCH_USER', payload: data })
+})
